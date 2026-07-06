72df626fbd4e16ee258ca46ce765b60912bf8107
AlfsAboltins <ingus-alfs.aboltins@edu.rtu.lv>
2026-07-07 02:28:21 +0300

    added design for homepage and created register

--- journal/views.py
+++ journal/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from . forms import CreateUserForm
 
 def home(request):
-    return render(request, 'journal/home.html')
+    return render(request, 'journal/index.html')
 
 def register(request):
-    return render(request, 'journal/register.html')
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+
+    else:
+        form = CreateUserForm()
+    context = {"CreateUserForm": form}
+    return render(request, 'journal/register.html', context)
+        
+            
+            
 
 def login(request):
     return render(request, 'journal/login.html')
